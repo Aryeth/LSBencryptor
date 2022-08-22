98852d5c64d96a9540b8ea08c39c552111178025
Aryeth <leo.bernard2002@gmail.com>
2022-08-22 10:19:22 +0200

    modified program to have 80 lines wide max

--- program/decrypt.py
+++ program/decrypt.py
@@ -3,10 +3,10 @@ from PIL import Image
 newImageName=f'./output/out.png'
 newImage=Image.open(newImageName)
 newPixels=newImage.load()
-##loading the image with the key inside
+##Loading the image with the key inside
 
 width, height=newImage.size
 for x in range(width):
     r,g,b=newPixels[x,0]
     print(chr(r),end = "")
-    ##TODO fix this (why tf is it in reverse kekW
+#Retreaving and decoding the key
--- program/encrypt.py
+++ program/encrypt.py
@@ -12,22 +12,26 @@ imgsrc=Image.open('input/in.png')
 pixels=imgsrc.load()
 width, height = imgsrc.size
 if(len(k2) > width):
-    print('the message you want to encode is too long ! It should be less than', width) 
-    ##since the program is pretty basic and that I want to keep it simple, I will not bother doing a 2nd line.
+    print('the message you want to encode is too long ! It should be less than'
+            , width) 
+    #since the program is pretty basic and that I want to keep it simple,
+    #I will not bother doing a 2nd line.
 
 a=0
-##a is a simple counter
+#a is a simple counter
 for k in k2: 
     pixel = list(imgsrc.getpixel((a, 0)))
     a+=1
     print(*pixel)
-##getting the pixel list of the first row of the image, goal is to have the pixel's rgb modified on the top row since this is the "easy" program.
-##note that this is a test on how to iterate using the base image.
+#getting the pixel list of the first row of the image, goal is to have the
+#pixel's rgb modified on the top row since this is the "easy" program.
+#note that this is a test on how to iterate using the base image.
 
 newImageName=f'./output/out.png'
 newImage=Image.new(imgsrc.mode, imgsrc.size)
 newPixels=newImage.load()
-##Creating a new image in the output, with the same pixels as the basic one, then loading it.
+#Creating a new image in the output, with the same pixels as the basic one,
+#then loading it.
 q=0
 for x in range(width):
     for y in range(height):
@@ -37,7 +41,8 @@ for x in range(width):
                 newPixels[x,y]=k2[q], g, b 
                 print('newpixel value: ', newPixels[x,y])
                 q+=1
-                ##changing the pixels of the first rows, putting the message on the red part of the pixel. I could let the old values for green and blue but maybe later cuz Im lazy.
+                #Changing the red values for the first line pixels to encode 
+                #the message, keeping original blue and green values.
             else:
                  newPixels[x,y]=imgsrc.getpixel((x,y))
         else:
@@ -50,4 +55,4 @@ test=''
 for j in k2:
     test+=(chr(j))
 print(test)
-##test print of the key.
+#Test print of the key.
